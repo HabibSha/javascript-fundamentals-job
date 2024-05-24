164b4a03c1cede917b65b16d95cdd43c9b29566c
habibsha <habibshahalahi7424@gmail.com>
2024-05-24 10:18:25 +0600

    problem solving no 11

--- problem-solving/codewars/first.js
+++ problem-solving/codewars/first.js
@@ -556,3 +556,99 @@ const firstNonRepeatingLetter = (s) =>
 // Differet way ---6
 const firstNonRepeatingLetter = (s) =>
   [...s].find((val) => !s.match(new RegExp(val, `gi`))[1]) || ``;
+
+// NO 11 String incrementer
+
+// Your job is to write a function which increments a string, to create a new string.
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+
+// Examples:
+// foo -> foo1
+// foobar23 -> foobar24
+// foo0042 -> foo0043
+// foo9 -> foo10
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+// Solution:
+function incrementString(strng) {
+  // Find the trailing number using regex
+  const numberPattern = /(\d+)$/;
+  const match = strng.match(numberPattern);
+
+  if (match) {
+    // Extract the number part and the rest of the string
+    const numberStr = match[1];
+    const prefix = strng.slice(0, -numberStr.length);
+
+    // Increment the number, preserving leading zeros
+    const incrementedNumber = (parseInt(numberStr, 10) + 1).toString();
+    const paddedNumber =
+      numberStr.slice(0, -incrementedNumber.length) + incrementedNumber;
+
+    return prefix + paddedNumber;
+  } else {
+    // If no trailing number, just append '1'
+    return strng + "1";
+  }
+}
+
+// Example usage:
+console.log(incrementString("foo")); // "foo1"
+console.log(incrementString("foobar23")); // "foobar24"
+console.log(incrementString("foo0042")); // "foo0043"
+console.log(incrementString("foo9")); // "foo10"
+console.log(incrementString("foo099")); // "foo100"
+// Different way ---1
+function incrementString(input) {
+  if (isNaN(parseInt(input[input.length - 1]))) return input + "1";
+  return input.replace(/(0*)([0-9]+$)/, function (match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
+
+// Different way ---2
+let incrementString = (str) =>
+  str.replace(/([0-8]|\d?9+)?$/, (e) => (e ? +e + 1 : 1));
+
+// Different way ---3
+const incrementString = (s) => s.replace(/[0-8]?9*$/, (m) => String(++m));
+
+// Different way ---4
+function incrementString(input) {
+  return input.replace(/([0-8]?)(9*)$/, function (s, d, ns) {
+    return +d + 1 + ns.replace(/9/g, "0");
+  });
+}
+
+// Different way ---5
+function incrementString(str) {
+  var c = str[str.length - 1];
+  switch (c) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+      return str.substring(0, str.length - 1) + (parseInt(c) + 1);
+    case "9":
+      return incrementString(str.substring(0, str.length - 1)) + 0;
+    default:
+      return str + "1";
+  }
+}
+
+// Different way ---6
+function incrementString(strng) {
+  return strng.replace(/[\d]*$/, (i) => String(+i + 1).padStart(i.length, 0));
+}
+
+// Different way ---7
+const incrementString = (strng) => strng.replace(/[0-8]?9*$/, (val) => ++val);
