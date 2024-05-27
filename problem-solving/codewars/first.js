63a10afca3d0d5a0eb9cbf754e07450b1aa503f4
habibsha <habibshahalahi7424@gmail.com>
2024-05-27 22:02:36 +0600

    Problem solving no 13. add different solution

--- problem-solving/codewars/first.js
+++ problem-solving/codewars/first.js
@@ -1018,3 +1018,26 @@ class PaginationHelper {
       : parseInt(itemIndex / this.itemsPerPage);
   }
 }
+
+// Different way ---4
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+  itemCount() {
+    return this.collection.length;
+  }
+  pageCount() {
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+  }
+  pageItemCount(pageIndex) {
+    if (pageIndex >= this.pageCount() || pageIndex < 0) return -1;
+    if (pageIndex !== this.pageCount() - 1) return this.itemsPerPage;
+    return this.collection.length % this.itemsPerPage;
+  }
+  pageIndex(itemIndex) {
+    if (itemIndex >= this.collection.length || itemIndex < 0) return -1;
+    return Math.floor(itemIndex / this.itemsPerPage);
+  }
+}
