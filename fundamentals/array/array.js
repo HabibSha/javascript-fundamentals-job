963b4f01c21afeadf281ef0389704b9deef81484
habibsha <habibshahalahi7424@gmail.com>
2024-06-28 22:43:27 +0600

    array methods

---
+++ fundamentals/array/array.js
@@ -0,0 +1,7 @@
+// ? Array methods:
+const arr1 = ["cars", "bikes", "motors", "volvo"];
+// arr1.sort();
+// console.log(arr1);
+// sort() method is alter the main array
+arr1.toSort();
+console.log(arr1);
